994c8a9030909078ae7808209a090ff40ba49b68
MArlon Cepeda <marlons.cepeda@gmail.com>
2023-07-09 00:48:40 -0500

    [FIX] reparado code smell en modules/shipment/routes

--- api/modules/shipment/routes.go
+++ api/modules/shipment/routes.go
@@ -3,11 +3,12 @@ package user
 import "github.com/gin-gonic/gin"
 
 func Routes(route *gin.Engine, ginFunc gin.HandlerFunc) {
+	shipmentHandler := NewShipmentHandler()
 
 	userRoute := route.Group("/user").Use(ginFunc)
 	{
-		userRoute.GET("/get/:id", NewShipmentHandler().GetUser)
-		userRoute.POST("/create/", NewShipmentHandler().CreateUser)
-		userRoute.PUT("/update/", NewShipmentHandler().UpdateUser)
+		userRoute.GET("/get/:id", shipmentHandler.GetUser)
+		userRoute.POST("/create/", shipmentHandler.CreateUser)
+		userRoute.PUT("/update/", shipmentHandler.UpdateUser)
 	}
 }
