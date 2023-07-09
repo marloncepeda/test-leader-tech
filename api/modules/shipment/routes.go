592b62e77e70d12b1295811f281866438885091a
MArlon Cepeda <marlons.cepeda@gmail.com>
2023-07-09 00:46:38 -0500

    [FIX] reparado error de llamadas en routes

--- api/modules/shipment/routes.go
+++ api/modules/shipment/routes.go
@@ -6,8 +6,8 @@ func Routes(route *gin.Engine, ginFunc gin.HandlerFunc) {
 
 	userRoute := route.Group("/user").Use(ginFunc)
 	{
-		userRoute.GET("/get/:id", GetUser)
-		userRoute.POST("/create/", CreateUser)
-		userRoute.PUT("/update/", UpdateUser)
+		userRoute.GET("/get/:id", NewShipmentHandler().GetUser)
+		userRoute.POST("/create/", NewShipmentHandler().CreateUser)
+		userRoute.PUT("/update/", NewShipmentHandler().UpdateUser)
 	}
 }
